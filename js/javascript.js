214bf6e7213b097a7c248696259b9dde63a484bb
Javiramos90 <javirm170290@gmail.com>
2024-07-30 14:53:08 +0200

    js

---
+++ js/javascript.js
@@ -0,0 +1,96 @@
+// // alert("Hola Mundo");
+
+// let nombre = "Javier te da la bienvenida";
+
+// console.log(nombre);
+
+// nombre = "Manuel";
+
+// console.log(nombre);
+
+// let precio = 36;
+
+// console.log("El precio es:" + precio);
+
+// precio = precio * 1.1;
+
+// console.log("precio actualizado:" + precio);
+
+// const nombre2 = "Palmera";
+
+// console.log(nombre2);
+
+// let cantidad = 2;
+// let precioProducto = 46.5;
+// let resultado = cantidad * precioProducto;
+
+// precio2 = "78";
+// resultado2 = precio2 + 8;
+
+// console.log(resultado2);
+
+// let nom = "Pepe";
+// let apellido = "Garcia";
+// console.log(nom + apellido);
+
+// let esfinde = false;
+// console.log(esfinde);
+
+// modulo
+// let cantidad1 = 7;
+// let cantidad2 = 2;
+// cantidad1= cantidad1 ++;
+// cantidad1 +=3;
+// console.log(cantidad1 % cantidad2);
+
+// console.log(cantidad1 > cantidad2);
+// console.log(cantidad1 < cantidad2);
+// // igual
+// console.log(cantidad1 == cantidad2);
+// // desigual
+// console.log(cantidad1 != cantidad2);
+
+// cantidad2-=4;
+// console.log(cantidad2);
+
+// let planta1 = "Cactus";
+// let planta2 = "cactus";
+// console.log(planta1 == planta2);
+
+// boleanos 
+
+// let estaSobrio = true;
+// let tieneCarnet = true;
+// let esMayor = true;
+// let puedeConducir = estaSobrio && tieneCarnet;
+// console.log("¿Puede conducir?" + "" + puedeConducir)
+
+// let esFinde = true;
+// let esVerano = false;
+// let voyDePaseo = esFinde || esVerano;
+// console.log("¿Voy de paseo?" + voyDePaseo);
+
+// let verdadero = true;
+// console.log(!verdadero);
+
+// let texto = "En un Lugar de la Mancha...";
+// let texto2 = "de Cuyo Nombre no quiero Acordarme"
+// // Longitud o cantidad de caracteres del string
+// console.log(texto.length);
+// console.log();
+// // Todos los caracteres en minuscula
+// console.log(texto.toLowerCase());
+// // Todos los caracteres en mayuscula
+// console.log(texto.toUpperCase());
+
+let nombre = "Manuel";
+let apellido = "Gonzalez";
+let edad = "40 años";
+console.log("Fulanito Fernández tiene tantos años como: " + nombre + " " + apellido + " tiene " + edad );
+console.log(`${nombre} ${apellido} tiene ${edad} años`);
+
+// string con salto de linea
+let poema = `Las rosas son rojas, 
+las violetas son azules, 
+javascript es divertido`;
+console.log(poema);
